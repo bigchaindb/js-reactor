02e7da8d3e98166e99904285953d39ed9990ba6c
Brett Sun <qisheng.brett.sun@gmail.com>
2016-05-20 13:26:21 +0200

    Upgrade ESLint configuration

--- server.demo.js
+++ server.demo.js
@@ -1,3 +1,5 @@
+/* eslint-disable strict, no-console */
+/* eslint-disable import/no-extraneous-dependencies, import/newline-after-import */
 'use strict';
 const path = require('path');
 
@@ -38,7 +40,8 @@ const server = new WebpackDevServer(compiler, {
 // Start server
 server.listen(PORT, HOST_NAME, (err) => {
     if (err) {
-        console.error(`Demo server ran into ${err} while starting on ${HOST_NAME}:${PORT}. Shutting down...`);
+        console.error(`Demo server ran into ${err} while starting on ${HOST_NAME}:${PORT}. ` +
+                      'Shutting down...');
         server.close();
     }
     console.log(`Demo server running on ${HOST_NAME}:${PORT}`);

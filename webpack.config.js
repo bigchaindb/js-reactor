2bd1bf22bf54801fd4166e8b77d555de61add6de
Brett Sun <qisheng.brett.sun@gmail.com>
2016-06-15 12:54:36 +0200

    Explicitly ask for sourceMaps from UglifyJS
    Otherwise breaks on webpack2-beta.9+

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,8 @@ const PROD_PLUGINS = [
         },
         output: {
             comments: false
-        }
+        },
+        sourceMap: true
     }),
     new webpack.LoaderOptionsPlugin({
         debug: false,
